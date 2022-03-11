44508adc748aece4422b7996ddb8ac7acc899408
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2022-03-10 18:57:42 -0800

    Read variables from ClimateNA output headers

--- climatena2netcdf.py
+++ climatena2netcdf.py
@@ -11,50 +11,65 @@ from trefoil.netcdf.variable import SpatialCoordinateVariables
 from netCDF4 import Dataset
 from pyproj import Proj
 
-VARIABLES = [
-    ('MAT', True), ('MWMT', True), ('MCMT', True), ('TD', True), ('MAP', False), ('MSP', False), ('AHM', True),
-    ('SHM', True), ('DD_0', False), ('DD5', False), ('DD_18', False), ('bFFP', False), ('eFFP', False), ('FFP', False),
-    ('PAS', False), ('EMT', True), ('EXT', True), ('Eref', False), ('CMD', False)
-]
+MULTIPLIERS = {
+    'MAT': 10,
+    'MWMT': 10,
+    'MCMT': 10,
+    'TD': 10,
+    'AHM': 10,
+    'SHM': 10,
+    'EMT': 10,
+    'EXT': 10,
+    'Tave_sm': 10,
+    'Tave_wt': 10,
+    'Tmin_sp': 10
+}
 
 
 @click.command()
 @click.argument('original_file', type=click.Path(exists=True))
 @click.argument('climatena_file', type=click.Path(exists=True))
 @click.argument('out_dir', type=click.Path(exists=True))
-def main(original_file, climatena_file, out_dir):
+@click.option('--variables', 'valid_variables', default=None)
+def main(original_file, climatena_file, out_dir, valid_variables):
     with rasterio.open(original_file) as ds:
         bounds = ds.bounds
-        affine = ds.profile['affine']
+        affine = ds.transform
         shape = ds.shape
 
     with open(climatena_file, 'r') as f_in:
         headers = csv.DictReader(f_in).fieldnames
+        variables = [x for x in headers if x not in ('ID1', 'ID2', 'Latitude', 'Longitude', 'Elevation')]
+
+    if valid_variables:
+        valid = [x.strip().lower() for x in valid_variables.split(',')]
+    else:
+        valid = variables
 
     print('Creating datasets...')
 
     grid = numpy.zeros(shape, dtype='int32')
     grid = numpy.ma.masked_where(grid == 0, grid)
 
-    for i, var in enumerate(VARIABLES):
+    for var in (x for x in variables if x.lower() in valid):
         out_path = os.path.join(
-            out_dir, '{}_{}.nc'.format(os.path.splitext(os.path.basename(climatena_file))[0], var[0])
+            out_dir, '{}_{}.nc'.format(os.path.splitext(os.path.basename(climatena_file))[0], var)
         )
 
         if os.path.exists(out_path):
             continue
 
         with Dataset(out_path, 'w', format='NETCDF4') as ds:
-            projection = Proj('+init=EPSG:4326')
+            projection = Proj('EPSG:4326')
             coord_vars = SpatialCoordinateVariables.from_bbox(
                 BBox(bounds, projection=projection), *reversed(grid.shape)
             )
             coord_vars.add_to_dataset(ds, 'longitude', 'latitude')
             data_var = ds.createVariable(
-                var[0], grid.dtype, dimensions=('latitude', 'longitude'), fill_value=grid.fill_value
+                var, grid.dtype, dimensions=('latitude', 'longitude'), fill_value=grid.fill_value
             )
             data_var[:] = grid
-            set_crs(ds, var[0], projection)
+            set_crs(ds, var, projection)
 
     print('Copying from ClimateNA data... (0%)', end='\r')
     with open(climatena_file, 'r') as f_in:
@@ -69,39 +84,38 @@ def main(original_file, climatena_file, out_dir):
             arr = numpy.loadtxt(
                 StringIO(lines),
                 delimiter=',',
-                usecols=[headers.index(x) for x in (['Latitude', 'Longitude'] + [v[0] for v in VARIABLES])]
+                usecols=[headers.index(x) for x in ['Latitude', 'Longitude'] + variables]
             )
             arr = numpy.moveaxis(arr, 1, 0)
 
             latitudes = arr[0]
             longitudes = arr[1]
 
-            for i, var in enumerate(VARIABLES):
-                out_path = os.path.join(
-                    out_dir, '{}_{}.nc'.format(os.path.splitext(os.path.basename(climatena_file))[0], var[0])
-                )
-                if os.path.exists(out_path):
-                    continue
+            for i, var in enumerate(variables):
+                if var.lower() in valid:
+                    out_path = os.path.join(
+                        out_dir, '{}_{}.nc'.format(os.path.splitext(os.path.basename(climatena_file))[0], var)
+                    )
 
-                variable = arr[i + 2]
+                    variable = arr[i + 2]
 
-                with Dataset(out_path, 'a') as ds:
-                    grid = ds.variables[var[0]][:]
-                    fill_value = grid.fill_value
-                    grid = grid.data
+                    with Dataset(out_path, 'a') as ds:
+                        grid = ds.variables[var][:]
+                        fill_value = grid.fill_value
+                        grid = grid.data
 
-                    for j, value in enumerate(variable):
-                        if value == -9999:
-                            continue
+                        for j, value in enumerate(variable):
+                            if value == -9999:
+                                continue
 
-                        col, row = [int(round(x)) for x in ~affine * (longitudes[j], latitudes[j])]
+                            col, row = [int(round(x)) for x in ~affine * (longitudes[j], latitudes[j])]
 
-                        if var[1]:
-                            value *= 10
+                            if var in MULTIPLIERS:
+                                value *= MULTIPLIERS[var]
 
-                        grid[row][col] = value
+                            grid[row][col] = value
 
-                    ds.variables[var[0]][:] = numpy.ma.masked_where(grid == fill_value, grid)
+                        ds.variables[var][:] = numpy.ma.masked_where(grid == fill_value, grid)
 
             print('Copying from ClimateNA data... ({}%)'.format(round(f_in.tell() / end * 100)), end='\r')
         print('Copying from ClimateNA data... (100%)')
